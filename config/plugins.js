68780cfd59e5e04f79f66bf64664aa2f36694df5
Pratap Singh <ps6067966@gmail.com>
2023-05-06 13:08:14 +0530

    new

--- config/plugins.js
+++ config/plugins.js
@@ -1,3 +1,4 @@
+// vercel
 module.exports = ({ env }) => ({
     "vercel-deploy": {
       enabled: true,
@@ -6,7 +7,7 @@ module.exports = ({ env }) => ({
           "https://api.vercel.com/v1/integrations/deploy/prj_Cm3nNdgAXYGBJ9JYU1s7AreYr7Ih",
         apiToken: "MYd9TJtw2RfxYibBBda2s5Nr",
         appFilter: "todo-backend",
-        teamFilter: "ps6067966",
+        teamFilter: "",
         roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
       },
     },
